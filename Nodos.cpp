20bd5613f4fd42e47f8645662c7315caf72f3916
AxelADN <tu.tamal.cocido@gmail.com>
2017-11-25 22:42:19 -0600

    Correccioon Nodos.cpp std::vector

--- Nodos.cpp
+++ Nodos.cpp
@@ -1,4 +1,6 @@
 #include "Nodos.hpp"
+#include <iostream>
+//#include "Test.hpp"
 
 std::map<vector,bool> Nodo::init_nodos_creados()
 {
@@ -22,6 +24,9 @@ Nodo::Nodo()
 	max_disparo = 1;
 	ciclo = false;
 
+	 
+
+
 }
 
 Nodo::Nodo(vector marcado0)
@@ -32,10 +37,14 @@ Nodo::Nodo(vector marcado0)
 	marcado = marcado0;
 	nodos++;
 	nodos_creados[marcado0] = false;
+
+	 
 }
 
 Nodo::Nodo(int max_disparo0, vector marcado_init)
 {
+
+	//vector *disparos0;
 	int ceros[max_disparo0] = {0};
 	mayorizado = false;
 	padre = NULL;
@@ -43,9 +52,14 @@ Nodo::Nodo(int max_disparo0, vector marcado_init)
 	marcado = marcado_init;
 	nodos = 1;
 	nodos_creados[marcado_init] = false;
+
 	disparos = new vector(ceros,max_disparo0);
+	//disparos = *disparos0;
+	hijos.resize(max_disparo0);
 	for(int i=0;i<max_disparo0;i++)
-		hijos[i]=NULL;
+		hijos.at(i)=NULL;
+
+	 
 
 }
 
@@ -57,6 +71,8 @@ Nodo::Nodo(Nodo *padre0, vector marcado0)
 	marcado = marcado0;
 	nodos++;
 	nodos_creados[marcado0] = false;
+
+	 
 }
 
 bool Nodo::nuevo_hijo(Nodo *nuevo_hijo0, int disparo)
@@ -64,10 +80,12 @@ bool Nodo::nuevo_hijo(Nodo *nuevo_hijo0, int disparo)
 	if(disparo<max_disparo)
 	{
 	    hijos[disparo] = nuevo_hijo0;
+	    
 	    return true;
 	}
 	else
 	{
+		
 	    return false;
 	}
 }
@@ -75,25 +93,35 @@ bool Nodo::nuevo_hijo(Nodo *nuevo_hijo0, int disparo)
 void Nodo::setPadre(Nodo *padre0)
 {
 	this->padre = padre0;
+
+	 
 }
 
 void Nodo::setCiclo(bool ciclo0)
 {
 	ciclo = ciclo0;
+
+	 
 }
 
 bool Nodo::getCiclo()
 {
+	 
+
 	return ciclo;
 }
 
 void Nodo::mayorizar()
 {
 	mayorizado = true;
+
+	 
 }
 
 bool Nodo::getMayorizado()
 {
+	 
+
 	return mayorizado;
 }
 
@@ -101,41 +129,58 @@ vector Nodo::getDisparos()
 {
 	for(int i=0; i<disparos.size(); i++)
 		hijos[i]==NULL ? disparos.set(i,0) : disparos.set(i,1);
+
+	 
+
 	return disparos;
 
 }
 
 int Nodo::getNodos()
 {
+	 
+
 	return nodos;
 }
 
 Nodo *Nodo::getPadre()
 {
+	 
+
 	return padre;
 }
 
 Nodo *Nodo::hijo(int disparo0)
 {
+	 
+
 	return hijos[disparo0];
 }
 
 std::vector<Nodo*> Nodo::getHijos()
 {
+	 
+
 	return hijos;
 }
 
 vector Nodo::getMarcado()
 {
+	 
+
 	return marcado;
 }
 
 void Nodo::setExplorado(vector marcado0, bool ciclo0)
 {
 	nodos_creados[marcado0] = ciclo0;
+
+	 
 }
 
 bool Nodo::getExplorado(vector marcado0)
 {
+	 
+
 	return nodos_creados[marcado0];
 }
