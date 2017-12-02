9bbda5a147edbaf5138bb0eadc7899f85309a509
CarSandoval <chcarlossandoval@gmail.com>
2017-12-01 20:26:18 -0600

    interfaz de usuario para prueba de la propiedad de reiniciado

--- ReiniciableTest.cpp
+++ ReiniciableTest.cpp
@@ -2,27 +2,54 @@
 #include"Arbol.h"
 //#include"Nodos.hpp"
 
-using namespace std;
+//using namespace std;
 
 int main()
 {
 	
-	int marcado[] = {1,0,0};
-	arbol tree(marcado,4,3);
+	int t,p;
+	int *marcado;
+
+	std::cout<<"Numero de transiciones: ";
+	std::cin>>t;
+	std::cout<<"Numero de Lugares: ";
+	std::cin>>p;
+	std::cout<<"Marcado inicial: ";
+	
+	//pido memoria para el marcado inicial
+	marcado = new int[p];
+	for(int i=0;i<p;i++)
+	{
+		std::cin>>marcado[i];	
+	}
+
+
+	//creo el arbol
+	arbol tree(marcado,t,p);
+
+	//auxiliar para la respuesta de reiniciabilidad
 	bool resp;
 
-	cout<<"estoy aqui"<<endl;
+	std::cout<<"Es acotado:? ";
+	if(tree.getAcotado())
+	{
+		std::cout<<"Si\n";
+	}
+	else
+	{
+		std::cout<<"No\n";
+	}
 
-	cout<<"es acotado:? "<<tree.getAcotado()<<endl;
+	//calculo reiniciabilidad
 	resp = tree.reiniciable();
 	
 	if(resp)
 	{
-		cout<<"es reiniciable"<<endl;
+		std::cout<<"Es reiniciable"<<std::endl;
 	}
 	else
 	{
-		cout<<"no es reiniciable"<<endl;
+		std::cout<<"No es reiniciable"<<std::endl;
 	}
 	
 	//tree = new arbol(marcado,4,3);
